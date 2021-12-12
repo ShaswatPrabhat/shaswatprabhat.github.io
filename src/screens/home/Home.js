5ec62148928a7996f3c92926e75ec8b2422cd938
Shaswat Prabhat <spd1238@gmail.com>
2021-12-12 18:28:15 +0530

    Introduce home styles file.

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,8 +1,9 @@
-import { LazyMotion, m } from 'framer-motion';
+import { LazyMotion } from 'framer-motion';
 import BurgerSideBar from '../../components/burger-side-bar/burger-side-bar';
 import { AppHeader, ScreenContainer, StyledAnchor } from '../app/App.styles';
 import { LINKS } from '../../utils/constants';
 import React from 'react';
+import { HomeContentContainer } from './Home.styles';
 
 const loadFeatures = () =>
   import('../../framer-motion-feature.js').then((res) => res.default);
@@ -23,19 +24,14 @@ export const Home = () => (
         <StyledAnchor to={LINKS.HOME_PATH}>Shaswat&apos;s blog</StyledAnchor>
       </AppHeader>
       <ScreenContainer>
-        <m.div
+        <HomeContentContainer
           initial={{ scale: 0.85 }}
           animate={{ scale: 1 }}
           transition={{ type: 'spring', duration: 1.8, bounce: 0.7 }}
-          style={{
-            alignSelf: 'center',
-            flex: 0.1,
-            width: '80%',
-          }}
         >
           <h2>Welcome to my blog!</h2>
           <p>Here I will share my musings and general Shaswat stuff</p>
-        </m.div>
+        </HomeContentContainer>
       </ScreenContainer>
     </LazyMotion>
   </div>
---
+++ src/screens/home/Home.styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { m } from 'framer-motion';
+
+export const HomeContentContainer = styled(m.div)`
+  align-self: center;
+  flex: 0.1;
+  width: 80%;
+`;
