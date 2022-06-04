1e370e06e240854a209e31dfe1efe35573c0770c
Shaswat Prabhat <spd1238@gmail.com>
2022-06-04 15:08:34 +0530

    Add transition background and Text colors.

--- src/screens/app/App.styles.js
+++ src/screens/app/App.styles.js
@@ -19,12 +19,32 @@ export const AppHeader = styled.header`
   justify-content: center;
   font-size: calc(10px + 2vmin);
 `;
+export const HbdHeader = styled.header`
+  min-height: 10vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 
 export const StyledAnchor = styled(m(Link))`
   font-size: 2rem;
   padding: 2rem 0;
   letter-spacing: 0.5rem;
   color: #61dafb;
+  text-decoration: none;
+  transition: color 0.3s linear;
+
+  @media (max-width: 576px) {
+    font-size: 1.5rem;
+    text-align: center;
+  }
+`;
+export const StyledHbdAnchor = styled(m(Link))`
+  font-size: 3rem;
+  padding: 2rem 0;
+  letter-spacing: 0.3rem;
+  color: #fcfbf7;
   text-decoration: none;
   transition: color 0.3s linear;
 
--- src/screens/hbdpnoki/HbdPnoki.js
+++ src/screens/hbdpnoki/HbdPnoki.js
@@ -1,11 +1,17 @@
 import { AnimatePresence, LazyMotion } from 'framer-motion';
-import { AppHeader, ScreenContainer, StyledAnchor } from '../app/App.styles';
+import { HbdHeader, ScreenContainer, StyledHbdAnchor } from '../app/App.styles';
 import { LINKS } from '../../utils/constants';
 import React, { useEffect } from 'react';
 import { HomeContentContainer } from './HbdPnoki.styles';
 
 const loadFeatures = () =>
   import('../../framer-motion-feature.js').then((res) => res.default);
+
+const animationProps = {
+  initial: { scale: 0.85 },
+  animate: { scale: 1 },
+  transition: { type: 'spring', duration: 1.8, bounce: 0.7 },
+};
 
 export const HbdPnoki = () => {
   useEffect(() => {
@@ -23,23 +29,46 @@ export const HbdPnoki = () => {
   }, []);
 
   return (
-    <div className="App">
+    <div
+      className="HbdPnoki"
+      style={{
+        background: `#0f0c29`,
+        // eslint-disable-next-line
+        background: `-webkit-linear-gradient(to right, #24243e, #302b63, #0f0c29)`,
+        // eslint-disable-next-line
+        background: `linear-gradient(to right, #24243e, #302b63, #0f0c29)`,
+      }}
+    >
       <LazyMotion features={loadFeatures}>
-        <AppHeader>
-          <StyledAnchor to={LINKS.HBD_PNOKI}>
+        <HbdHeader>
+          <StyledHbdAnchor to={LINKS.HBD_PNOKI} {...animationProps}>
             Happy Birthday Pnoki!!
-          </StyledAnchor>
-        </AppHeader>
+          </StyledHbdAnchor>
+        </HbdHeader>
         <ScreenContainer>
           <AnimatePresence>
-            <HomeContentContainer
-              initial={{ scale: 0.85 }}
-              animate={{ scale: 1 }}
-              transition={{ type: 'spring', duration: 1.8, bounce: 0.7 }}
-            >
-              <h2>Dhin Chak!</h2>
-              <h2>Dhin Chak!!</h2>
-              <h2>Dhin dhin dhaaa!</h2>
+            <HomeContentContainer {...animationProps}>
+              <h2
+                style={{
+                  color: '#faf5e3',
+                }}
+              >
+                Dhin Chak!
+              </h2>
+              <h2
+                style={{
+                  color: '#faf1d2',
+                }}
+              >
+                Dhin Chak!!
+              </h2>
+              <h2
+                style={{
+                  color: '#f7ebc1',
+                }}
+              >
+                Dhin dhin dhaaa!
+              </h2>
             </HomeContentContainer>
           </AnimatePresence>
         </ScreenContainer>
