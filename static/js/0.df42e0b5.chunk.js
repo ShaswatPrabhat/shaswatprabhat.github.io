6b31c1b3b5285fc35a2a77f81a2b0008364a45b4
Shaswat Prabhat <spd1238@gmail.com>
2022-06-06 17:20:23 +0530

    Updates

--- static/js/0.df42e0b5.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpblog=this.webpackJsonpblog||[]).push([[0],{113:function(s,t,a){"use strict";a.r(t);var o=a(115);t.default=o.a}}]);
-//# sourceMappingURL=0.df42e0b5.chunk.js.map
---
+++ static/js/0.ebdf99f1.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpblog=this.webpackJsonpblog||[]).push([[0],{115:function(s,t,a){"use strict";a.r(t);var o=a(117);t.default=o.a}}]);
+//# sourceMappingURL=0.ebdf99f1.chunk.js.map
